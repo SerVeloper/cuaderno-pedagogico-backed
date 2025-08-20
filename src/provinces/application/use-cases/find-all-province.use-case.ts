15c9e8a321c3f9e799700c2b89345d20500535b2
seVeloper <sergioveloper@gmail.com>
2025-08-19 22:00:54 -0400

    se quitaron swagger de casos de uso para mantenerlos solo en el controlador

--- src/provinces/application/use-cases/find-all-province.use-case.ts
+++ src/provinces/application/use-cases/find-all-province.use-case.ts
@@ -1,7 +1,6 @@
 import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { ProvincesRepositoryInterface } from '../../domain/interfaces/province.repository.interface';
 import { ProvinceEntity } from '../../domain/entities/province.entity';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Injectable()
 export class FindAllProvincesUseCase {
@@ -10,8 +9,6 @@ export class FindAllProvincesUseCase {
     private readonly provinceRepositoryInterface: ProvincesRepositoryInterface,
   ) {}
 
-  @ApiOperation({ summary: 'Find all provinces' })
-  @ApiResponse({ status: 200, description: 'List of provinces', type: [ProvinceEntity] })
   async execute(): Promise<ProvinceEntity[]> {
     try {
       return await this.provinceRepositoryInterface.findAll();
