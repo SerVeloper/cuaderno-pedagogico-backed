76b4035f69005f1ff081d9f0a38eeb939b48cb59
sebastianDLL <delgadillosebastian75@gmail.com>
2025-08-25 21:10:43 -0400

    swagger en los dtos

--- src/modules/levels/application/dtos/update-level.dto.ts
+++ src/modules/levels/application/dtos/update-level.dto.ts
@@ -1,12 +1,24 @@
 import { IsString, IsOptional, IsBoolean, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateLevelDto {
   @IsOptional()
   @IsString()
   @MaxLength(255)
+  @ApiProperty({
+    example: 'This is a description of the level',
+    description: 'A brief description of the level',
+    maxLength: 255,
+    required: false,
+  })
   Description?: string;
 
   @IsOptional()
   @IsBoolean()
+  @ApiProperty({
+    example: true,
+    description: 'Indicates whether the level is active',
+    required: false,
+  })
   IsActive?: boolean;
 }
